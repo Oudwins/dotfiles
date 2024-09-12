87fc482a77368cec817c2227b10d591f9f50a049
oudwins <tm@tristanmayo.com>
2024-09-12 13:04:32 +0200

    trying to add unstable, didn't work

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,10 +10,18 @@
       url = "github:NixOS/nixpkgs/nixpkgs-unstable";
       flake = false;
     };
+    # sops-nix = {
+    #   url = "github:mic92/sops-nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = { nixpkgs, home-manager, nixos-hardware, unstable, ... }: 
   let 
+  #  forAllSystems = lib.genAttrs [
+  #       "x86_64-linux"
+  #       #"aarch64-darwin"
+  #   ];
     system = "x86_64-linux";
     overlay_freetube = final: prev: {
       # Override a specific package, e.g., "example-package"
@@ -23,6 +31,7 @@
       go = unstable.lib.go;
     };
     overlays = [overlay_golang overlay_freetube];
+    inherit unstable;
     # pkgs = import nixpkgs {
     # config = {
     # allowUnfree = true;
@@ -33,29 +42,63 @@
     # # Apply patches, set a pkgs to an older version... (here you can do this)
     # };
 
-
-
     lib = nixpkgs.lib;
   in {
+   ################### DevShell ####################
+      #
+      # Custom shell for bootstrapping on new hosts, modifying nix-config, and secrets management
+
+      # devShells = forAllSystems (
+      #   system:
+      #   let
+      #     pkgs = nixpkgs.legacyPackages.${system};
+      #   in
+      #   {
+      #     default = pkgs.mkShell {
+      #       NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+
+      #       inherit (self.checks.${system}.pre-commit-check) shellHook;
+      #       buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+
+      #       nativeBuildInputs = builtins.attrValues {
+      #         inherit (pkgs)
+
+      #           nix
+      #           home-manager
+      #           git
+      #           just
+
+      #           age
+      #           ssh-to-age
+      #           sops
+      #           ;
+      #       };
+      #     };
+      #   }
+      # );
+
+    # SYSTEMS
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        inherit system;
-        modules = [
-        nixos-hardware.nixosModules.lenovo-thinkpad-p14s-amd-gen2
-        ./system/configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.tmx = {
-          imports = [ ./home.nix ];
+          inherit system;
+          modules = [
+          nixos-hardware.nixosModules.lenovo-thinkpad-p14s-amd-gen2
+          ./system/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.tmx = {
+            imports = [ ./home.nix ];
+            };
+            home-manager.extraSpecialArgs = {
+              inherit unstable;
           };
-        }
+          }
         ];
-      };
-      specialArgs = {
-          inherit overlays; 
-        };
+        # specialArgs = {
+        #     inherit overlays; 
+        #   };
     };
-
   };
+};
 }
