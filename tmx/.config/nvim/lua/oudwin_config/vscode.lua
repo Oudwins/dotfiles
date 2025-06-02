4f1d8f4d99e186e764f647231cffcc208a318b39
tristan <tm@tristanmayo.com>
2025-06-02 08:02:29 +0200

    nvim: updated bindings

--- tmx/.config/nvim/lua/oudwin_config/vscode.lua
+++ tmx/.config/nvim/lua/oudwin_config/vscode.lua
@@ -7,6 +7,6 @@ vim.keymap.set('n', "[d", function() vscode.call("editor.action.marker.prev") en
 -- [p]roject [s]earch
 vim.keymap.set('n', "<leader>ps", function() vscode.call("workbench.action.findInFiles") end, { silent = true })
 -- [v]ariable [r]e[n]ame
-vim.keymap.set("n", "<leader>vrf", function() vscode.call("editor.action.rename") end, { silent = true })
+vim.keymap.set("n", "<leader>vrn", function() vscode.call("editor.action.rename") end, { silent = true })
 -- [v]ariable [r]e[f]erences
 vim.keymap.set("n", "<leader>vrf", function() vscode.call("references-view.findReferences") end, { silent = true })
