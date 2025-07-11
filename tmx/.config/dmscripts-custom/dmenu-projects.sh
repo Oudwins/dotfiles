d19117cc46b4718dec58a06c7202d3e0b8be62c9
tristan <tm@tristanmayo.com>
2025-07-11 23:09:10 +0200

    feat: quick open for parent dirs

--- tmx/.config/dmscripts-custom/dmenu-projects.sh
+++ tmx/.config/dmscripts-custom/dmenu-projects.sh
@@ -3,8 +3,9 @@
 DMENU=${DMENU:-dmenu}
 EDITOR=${CODE_EDITOR:-code}
 
-# List of parent directories to search. This comes from CODE_PROJECT_DIRS env variable defined in home.nix
-IFS=':' read -r -a PARENT_DIRS <<< "$CODE_PROJECT_DIRS"
+# List of parent directories to search. This comes from CODE_PROJECTS_PARENT_DIRS env variable defined in home.nix
+IFS=':' read -r -a PARENT_DIRS <<< "$CODE_PROJECTS_PARENT_DIRS" # project source directories
+IFS=':' read -r -a PROJECT_DIRS <<< "$CODE_PROJECTS" # specific project directories
 
 # Command to run in the selected directory
 RUN_COMMAND="cd"
@@ -16,8 +17,17 @@ for dir in "${PARENT_DIRS[@]}"; do
     directories+=$'\n'  # Add a newline between parent directories
 done
 
+# Add individual project directories
+for dir in "${PROJECT_DIRS[@]}"; do
+    if [ -d "$dir" ]; then
+        dirname=$(basename "$dir")
+        parentdir=$(dirname "$dir")
+        directories+="$dirname\t$parentdir\n"
+    fi
+done
+
 # Use dmenu to fuzzy find and select a directory
-selected=$(echo "$directories" | $DMENU -i -l 15 -p "Select directory:")
+selected=$(echo -e "$directories" | $DMENU -i -l 15 -p "Select directory:")
 
 if [ -n "$selected" ]; then
     # Extract the child directory name and parent directory
