db28776831bdbe238b1164d7fefc7745f99da109
oudwins <tm@tristanmayo.com>
2026-03-19 07:57:03 +0100

    feat: install beads

--- nixos/home/common/agents/default.nix
+++ nixos/home/common/agents/default.nix
@@ -1,10 +1,16 @@
-{ config, pkgs, lib, ... }@args: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}@args:
+{
   imports = [ ];
   # We don't install opencode, claude code or codex here because these packages are updated a lot so would prefer to just install manually
 
-  home.packages = with pkgs;
-    [
-      # Opencode for code linting
-      typescript-language-server
-    ];
+  home.packages = with pkgs; [
+    beads
+    # Opencode for code linting
+    typescript-language-server
+  ];
 }
