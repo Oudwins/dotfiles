69c605c375fec42c76782eaf83b22e8ad3a414f1
oudwins <tm@tristanmayo.com>
2026-05-16 06:45:26 +0200

    chore

--- nixos/home/common/agents/default.nix
+++ nixos/home/common/agents/default.nix
@@ -2,7 +2,6 @@
   imports = [ ];
   # We don't install opencode, claude code or codex here because these packages are updated a lot so would prefer to just install manually
 
-
   home.packages = with pkgs; [
     jq
     bun
@@ -16,17 +15,17 @@
   # Local AI
   # ollama
   # ollama run qwen3:8b
-  ''
-    ram  model
-    8–16GB qwen3:4b
-    16–24GB qwen3:8b
-    32–36GB qwen3:14b or qwen3:32b
-
-    ---
-    Setup
-    -----
-    OLLAMA_CONTEXT_LENGTH=32768 ollama serve
-    ollama run qwen3:8b # only first time. To pull the model. You can also do ollama pull {model}
-    ollama run qwen3-coder:30b
-  '';
+  # ''
+  #   ram  model
+  #   8–16GB qwen3:4b
+  #   16–24GB qwen3:8b
+  #   32–36GB qwen3:14b or qwen3:32b
+  #
+  #   ---
+  #   Setup
+  #   -----
+  #   OLLAMA_CONTEXT_LENGTH=32768 ollama serve
+  #   ollama run qwen3:8b # only first time. To pull the model. You can also do ollama pull {model}
+  #   ollama run qwen3-coder:30b
+  # '';
 }
